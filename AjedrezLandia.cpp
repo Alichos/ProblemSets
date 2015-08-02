a27c9131a9755fc88b33c051a14a74151b5e7bd0
Alichos <Alichos@users.noreply.github.com>
2015-08-02 17:25:33 -0500

    Create AjedrezLandia.cpp

--- AjedrezLandia.cpp
+++ AjedrezLandia.cpp
@@ -4,7 +4,6 @@ using namespace std;
 const int MAXN = 9;
 const int INF = (1 << 30);
 
-int ope = 0;
 int n,m,k;
 int mayor = INF;
 char mapa[MAXN * MAXN];
@@ -35,43 +34,49 @@ int adyacentes(const string& lineal, const int& posicion){
     int res = 0;
     for(int j = 0; j < 4; j++){
         int sig_pos = posicion + dir[j];
+
+        if(sig_pos < 0 || sig_pos >=  n * m) continue;
         if(posicion % m == 0 && dir[j] == -1) continue;
         if(posicion % m == m - 1 && dir[j] == 1) continue;
-        
+
         if(lineal[sig_pos] == 'T')
             res++;
     }
     return res;
 }
 
-int completo(string lineal,int posicion){
-    int ocupadas = k;
+int completo(string lineal){
+    string query = lineal;
+    for(int i = 0; i < n * m; i++) query[i] = 'N';
+
     for(int i = 0; i < n * m; i++){
-        if(lineal[i] == '#' || 
-           lineal[i] == '.' || 
-           lineal[i] == 'S') 
-            continue;
+        if(lineal[i] == '#' || lineal[i] == '.' || lineal[i] == 'S') continue;
 
         if(lineal[i] == 'T'){
-            ocupadas++;
+            query[i] = 'S';
             for(int j = 0; j < 4; j++){
-                int sig_pos = i + dir[j];
-                while(sig_pos >= 0 && sig_pos < n * m){
-                    if(dir[j] == 1 && sig_pos % m == m - 1) break;
-                    if(dir[j] == -1 && sig_pos % m == 0) break;
+                int pos_actual = i + dir[j];
 
-                    if(lineal[sig_pos] == 'T') return -1;
+                if(i % m == m - 1 && dir[j] == 1) continue;
+                if(i % m == 0 && dir[j] == -1) continue;
 
-                    if(lineal[sig_pos] == 'S'){
-                        sig_pos += dir[j];
-                        continue;
+                while(true){
+                    if(pos_actual < 0 || pos_actual >= n * m) break;
+
+                    if(lineal[pos_actual] == 'T') return -1;
+
+                    if(lineal[pos_actual] == '.' || lineal[pos_actual] == 'S'){
+                        lineal[pos_actual] = 'S';
+                        query[pos_actual] = 'S';
+                    } else {
+                        query[pos_actual] = 'S';
+                        break;
                     }
 
-                    if(lineal[sig_pos] != '.') break;
+                    if(pos_actual % m == m - 1 && dir[j] == 1) break;
+                    if(pos_actual % m == 0 && dir[j] == -1) break;
 
-                    lineal[sig_pos] = 'S';
-                    sig_pos += dir[j];
-                    ocupadas++;
+                    pos_actual += dir[j];
                 }
             }
         }
@@ -82,20 +87,24 @@ int completo(string lineal,int posicion){
             if(adyacen < lineal[i] - 'A') return 0;
         }
     }
-    if(ocupadas == n * m) return 1;
-    return 0;
+
+    for(int i = 0; i < n * m; i++)
+        if(lineal[i] == '.' || query[i] == 'N') return 0;
+    return 1;
 }
 
 void busqueda(string lineal, int x){
-    if(x >= n * m) return;
+    if(x > n * m) return;
 
-    int query = completo(lineal,x);
+    int query = completo(lineal);
+
     if(query == -1) return;
-    
+
     if(query == 1){
         mayor = min(mayor, cuenta_torres(lineal));
         return;
     }
+
 
     bool flag = false;
     if(x - m >= 0 && lineal[x - m] >= 'A' && lineal[x - m] <= 'E'){
@@ -140,6 +149,11 @@ int main(){
 
     string raiz(mapa);
     busqueda(raiz,0);
+
+    if(mayor == INF){
+        cout << 0 << "\n";
+        return 0;
+    }
 
     cout << mayor << "\n";
     return 0;
