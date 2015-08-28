f74f8fd76b73291886a4d1b0c5875a16ac586ceb
Alichos <Alichos@users.noreply.github.com>
2015-08-27 20:00:28 -0500

    marca.cpp

---
+++ Marca.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n,m;
+    cin >> n >> m;
+    
+    map<pair<int,int>,int> vis;
+    
+    for(int i = 1; i <= 10; i++)
+        vis[make_pair(1,i)] = i;
+    
+    vis[make_pair(2,2)] = 3;
+    for(int i = 3; i <= 10; i++)
+        vis[make_pair(2,i)] = i + 2;
+        
+    for(int i = 3; i <= 10; i++)
+        vis[make_pair(3,i)] = (3 * i) - i + 1;
+    
+    vis[make_pair(4,4)] = 11;
+    vis[make_pair(4,5)] = 14;
+    vis[make_pair(4,6)] = 16;
+    vis[make_pair(4,7)] = 19;
+    vis[make_pair(4,8)] = 21;
+    vis[make_pair(4,9)] = 24;
+    vis[make_pair(4,10)] = 26;
+    
+    n = min(n,m);
+    m = max(n,m);
+    
+    if(n <= 4)
+        cout << vis[make_pair(n,m) << "\n";
+    
+    int res = (n * 2) + (m * 2) - 4;
+    n -= 4;
+    m -= 4;
+    
+    res += vis[make_pair(n,m)];
+    
+    cout << res << "\n";
+    return 0;
+}
