dcf62da219012613298cd410525e0b135d784046
Alichos <Alichos@users.noreply.github.com>
2015-11-14 13:26:55 -0600

    Update LazyPropagation.cpp

--- LazyPropagation.cpp
+++ LazyPropagation.cpp
@@ -1,12 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+const int INF = (1 << 30); 
 const int MAXN = 100002;
 
 int n;
 struct SegmentTree{
     int lazy[MAXN * 4];
     int tree[MAXN * 4];
+    
+    void construye(int nodo, int izq, int der){
+        if(izq > der)
+            return;
+            
+        if(izq == der){
+            tree[nodo] = INF;
+            return;
+        }
+        
+        int mitad = (izq + der) / 2;
+        construye(nodo * 2, izq, mitad);
+        construye((nodo * 2) + 1, mitad + 1, der);
+    }
+    
     void update(int nodo, int izq, int der, int i, int j, int valor){
         if(lazy[nodo] > 0){
             tree[nodo] += lazy[nodo];
@@ -39,6 +55,33 @@ struct SegmentTree{
     
     void update_(int i, int j, int valor){
         update(1, 1, n - 1, i, j, valor);
+    }
+    
+    int query(int nodo, int izq, int der, int i, int j){
+        if(izq > der || j < izq || i > der)
+            return -inf;
+        
+        if(lazy[nodo] != 0){
+            tree[nodo] += lazy[nodo];
+            if(a != b){
+                lazy[nodo * 2] = lazy[nodo];
+                lazy[(nodo * 2) + 1] = lazy[nodo];
+            }
+            lazy[nodo] = 0;
+        }
+        
+        if(izq >= i && der <= j)
+            return tree[nodo];
+        
+        int mitad = (izq + der) / 2;
+        int hijoIzq = query(nodo * 2, izq, mitad, i, j);
+        int hijoDer = query((nodo * 2) + 1, mitad + 1, der, i, j);
+        
+        return max(hijoIzq, hijoDer);
+    }
+    
+    int query_(int i, int j){
+        query(1, 1, n - 1, i, j);
     }
 };
 
